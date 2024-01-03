f91b4920a57eff09296c688a63b62dc313128f48
Alex <alex@Aleksandrs-MacBook-Pro-2.local>
2024-01-03 20:15:54 +0200

    First commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  --add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim" },
+  { "rmehri01/onenord.nvim" },
+  { "AlexvZyl/nordic.nvim" },
+  { "folke/tokyonight.nvim" },
+  { "rose-pine/neovim", name = "rose-pine" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onenord",
+    },
+  },
+}
